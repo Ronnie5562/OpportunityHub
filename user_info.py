1d43d49106e3a0b44d9f6d4b7753d64b7d5e4917
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-08-02 17:08:59 +0200

    Some updates

--- user_info.py
+++ user_info.py
@@ -10,7 +10,7 @@ with open('users.csv', 'a') as file:
     file.write(f"{firstname}, {lastname}, {email}, {job_preference}")
 
 
-While True:
+while True:
     try:
         experience_level = int(input('How many years of experience do you have? '))
     except (ValueError, TypeError):
@@ -22,6 +22,7 @@ While True:
                 print('Welcome to the opportunityHub mentors platform')
                 with open('mentors.csv', 'a') as file:
                     file.write(f"{firstname}, {lastname}, {email}, {job_preference}")
+                break
 
         else:
             mentee = input('Do you need a mentor? ')
