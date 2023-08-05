0bd8d12d421c3653943e23222c131161e8f99ddb
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-08-05 13:31:08 +0200

    Building

--- user_info.py
+++ user_info.py
@@ -1,32 +1,63 @@
-firstname = input('Enter your first name: ')
-
-lastname = input('Enter your last name: ')
-
-email = input('Enter your email: ')
-
-job_preference = input('What kind of job do you want? ')
-
-with open('users.csv', 'a') as file:
-    file.write(f"{firstname}, {lastname}, {email}, {job_preference}")
+def ask_for_action():
+    # The entry point of the program
+    while True:
+        print('Do you have an account? ')
+        print('1. SignUp: ')
+        print('2. SignIn: ')
+        option = input('')
 
 
-while True:
+def get_user_details():
+    firstname = input('Enter your first name: ')
+    lastname = input('Enter your last name: ')
+    email = input('Enter your email: ')
+    job_preference = input('What kind of job do you want? ')
+    return firstname, lastname, email, job_preference
+
+
+def save_to_users_csv(firstname, lastname, email, job_preference):
+    with open('users.csv', 'a') as file:
+        file.write(f"\n{firstname}, {lastname}, {email}, {job_preference}")
+
+
+def is_integer(value):
     try:
-        experience_level = int(input('How many years of experience do you have? '))
-    except (ValueError, TypeError):
-        print('invalid input')
+        int(value)
+        return True
+    except ValueError:
+        return False
+
+
+def get_experience_level():
+    while True:
+        experience_level = input('How many years of experience do you have? ')
+        if is_integer(experience_level):
+            return int(experience_level)
+        print('Invalid input. Please enter a valid number.')
+
+
+def main():
+    firstname, lastname, email, job_preference = get_user_details()
+    save_to_users_csv(firstname, lastname, email, job_preference)
+
+    experience_level = get_experience_level()
+    if experience_level >= 5:
+        mentorship_request = input('Do you want to be a mentor? [Yes / No]')
+        if mentorship_request.lower() == 'yes':
+            print('Welcome to the opportunityHub mentors platform')
+            # Save mentor details similarly to "mentors.csv"
+            with open('mentors.csv', 'a') as file:
+                file.write(
+                    f"\n{firstname}, {lastname}, {email}, {job_preference}, {experience_level}")
     else:
-        if (experience_level >= 5):
-            mentorship_request = input('Do you want to be a mentor? [Yes / No]')
-            if (mentorship_request == 'Yes'):
-                print('Welcome to the opportunityHub mentors platform')
-                with open('mentors.csv', 'a') as file:
-                    file.write(f"{firstname}, {lastname}, {email}, {job_preference}")
-                break
+        mentee_request = input('Do you need a mentor? [Yes / No]')
+        # Function to handle mentee request
 
-        else:
-            mentee = input('Do you need a mentor? ')
-            # function
+    print('Thank you for joining opportunityHub')
 
 
-print('Thank you for joining opportunityHub')
+if __name__ == "__main__":
+    main()
+
+# Bernice, Akudbilla, b.akudbilla@alustudent.com, Data analyst
+# Ronald, Abimbola, r.abimbola@alustudent.com, Software engineer
